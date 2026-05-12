b2b1ddb47e0edc5499cd02f029fcf02bb93f2f3c
George482-git <gaoxinghe482@gmail.com>
2026-05-12 18:05:07 +0800

    first commit

---
+++ src/main/java/com/xinghe/gittest/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.xinghe.gittest.service.impl;
+
+
+import com.xinghe.gittest.service.UserService;
+
+/**
+ * @Author: Xinghe
+ * @Date: 2026/5/12 18:03
+ */
+
+public class UserServiceImpl implements UserService {
+}
